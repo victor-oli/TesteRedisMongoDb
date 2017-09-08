63a42c02bfc1da429f7c2d8808cda03456d50d67
João Victor de Oliveira Espinosa <jvespinosa@stone.com.br>
2017-09-08 14:59:46 -0300

    ajustes no exemplo feito no console.

--- TesteRedisMongoDb.Ui/Program.cs
+++ TesteRedisMongoDb.Ui/Program.cs
@@ -1,37 +1,118 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using Newtonsoft.Json;
+using System;
 using System.Linq;
-using MongoDB.Driver;
 using TesteRedisMongoDb.Mongo;
 using TesteRedisMongoDb.Redis;
 
 namespace TesteRedisMongoDb.Ui
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			ProdutoInMemory galaxy8NaMemoria = ProdutoInMemoryDao.GetByNome("Galaxy 8");
-			Produto galaxy8NoMongo = null;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Digite (B) para buscar e (C) para cadastrar: ");
 
-			if (galaxy8NaMemoria == null)
-			{
-				var collection = new MongoManager<Produto>().open("Produto");
+                string option = Console.ReadLine();
 
-				galaxy8NoMongo = collection.Find(x => x.Nome.Equals("Galaxy 8")).FirstOrDefault();
+                if (!string.IsNullOrWhiteSpace(option))
+                {
+                    if (option.ToUpper().Equals("B"))
+                    {
+                        while (true)
+                        {
+                            Console.Write("Digite o nome do produto: ");
 
-				galaxy8NaMemoria = new ProdutoInMemory
-				{
-					Id = galaxy8NoMongo.Id.ToString(),
-					Nome = galaxy8NoMongo.Nome,
-					Valor = galaxy8NoMongo.Valor
-				};
+                            string nomeDoProduto = Console.ReadLine();
 
-				ProdutoInMemoryDao.Set(galaxy8NaMemoria);
-			}
+                            if (!string.IsNullOrWhiteSpace(nomeDoProduto))
+                            {
+                                ProdutoInMemory produto = BuscarProduto(nomeDoProduto);
 
-			Console.WriteLine($"{galaxy8NaMemoria.Nome} - {galaxy8NaMemoria.Valor}");
+                                if (produto != null)
+                                    Console.WriteLine("Produto: " + JsonConvert.SerializeObject(produto));
+                                else
+                                    Console.WriteLine("O produto digitado não existe.");
 
-			Console.ReadKey();
-		}
-	}
+                                break;
+                            }
+                            else
+                                break;
+                        }
+                    }
+                    else if (option.ToUpper().Equals("C"))
+                    {
+                        while (true)
+                        {
+                            Console.Write("Digite o nome do produto: ");
+
+                            string nomeDoProduto = Console.ReadLine();
+
+                            Console.Write("Digite o valor do produto: ");
+
+                            decimal valorDoProduto = Convert.ToDecimal(Console.ReadLine());
+
+                            CadastrarProduto(nomeDoProduto, valorDoProduto);
+
+                            Console.WriteLine("Produto cadastrado com sucesso.");
+
+                            break;
+                        }
+                    }
+                    else
+                        break;
+                }
+                else
+                    break;
+            }
+        }
+
+        private static void CadastrarProduto(string nome, decimal valor)
+        {
+            IMongoCollection<Produto> collection = new MongoManager<Produto>().open("Produto");
+            Produto produto = new Produto
+            {
+                Nome = nome,
+                Valor = valor
+            };
+
+            collection.InsertOne(produto);
+
+            var produtoNaMemoria = new ProdutoInMemory
+            {
+                Id = produto.Id.ToString(),
+                Nome = produto.Nome,
+                Valor = produto.Valor
+            };
+
+            ProdutoInMemoryDao.Set(produtoNaMemoria);
+        }
+
+        private static ProdutoInMemory BuscarProduto(string nome)
+        {
+            ProdutoInMemory produtoNaMemoria = ProdutoInMemoryDao.GetByNome(nome);
+
+            if (produtoNaMemoria == null)
+            {
+                IMongoCollection<Produto> collection = new MongoManager<Produto>().open("Produto");
+                Produto produto = collection.Find(x => x.Nome.Equals(nome)).FirstOrDefault();
+
+                if (produto == null)
+                    return null;
+
+                produtoNaMemoria = new ProdutoInMemory
+                {
+                    Id = produto.Id.ToString(),
+                    Nome = produto.Nome,
+                    Valor = produto.Valor
+                };
+
+                ProdutoInMemoryDao.Set(produtoNaMemoria);
+            }
+
+            return produtoNaMemoria;
+        }
+    }
 }
